d259e2f7fa65bdc036f79e9a9dec6f3f67d43615
Redmoogle <dakotamew@gmail.com>
2022-06-24 08:18:20 -0400

    Update mp3stream.py

--- mp3stream.py
+++ mp3stream.py
@@ -3,9 +3,9 @@ import urllib.request as urlreq
 import socket
 import threading
 import time
-import math
 import logging
 import mp3packet
+import subprocess
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -22,10 +22,13 @@ next = 0
 
 url = "https://hitradio-maroc.ice.infomaniak.ch/hitradio-maroc-128.mp3"
 
+# MP3 Chunks to OPUS Chunks
+cmd = "ffmpeg -i stream.mp3 -b:a 48k processed.opus"
+
 def on_new_client(conn, addr):
     global to_add
     conn.send(bytes('HTTP/1.1 200 OK\r\n', 'utf-8')) # HTTP requests expect a 200 OK before any header or data
-    conn.send(bytes("Content-Type: audio/mpeg\n\n", 'utf-8')) # Specify its a mp3 stream
+    conn.send(bytes("Content-Type: audio/opus\n\n", 'utf-8')) # Specify its a mp3 stream
     to_add.add(conn)
 
 def reconnect():
@@ -100,7 +103,7 @@ def bufferio():
     logging.info("Beginning MP3 Relay Playback")
     while(True):
         try:
-            buffer = extconn.read(next) # Recieve a mp3 packet
+            buffer = extconn.read(next*45) # Recieve a mp3 packet
             if(buffer == b""):
                 logging.warning('Detecting empty data stream... Reconnecting')
                 reconnect()
@@ -110,7 +113,11 @@ def bufferio():
             reconnect()
             continue
 
-        handle_clients(buffer)
+        with open("stream.mp3", "wb+") as filebuffer:
+            filebuffer.write(buffer)
+        subprocess.call(cmd, shell=True)
+        with open("processed.opus", "rb") as filebuffer:
+            handle_clients(filebuffer.read())
 
 def relay_start():
     iothread = threading.Thread(target=bufferio)
